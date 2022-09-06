9eef2434267baf8ee5e75737ed3da8fd3b53da5d
Marymezh <maria.mezhova@yahoo.com>
2022-09-06 17:47:35 +0700

    setting up table view

--- FileManagerStoryboard/MyFoldersTableViewController.swift
+++ FileManagerStoryboard/MyFoldersTableViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class MyFoldersTableViewController: UITableViewController {
-
+    
+    private var fileManager = FileManager.default
+    private lazy var url = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    private var listOfContents: [URL] {
+        return (try? FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)) ?? []
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     @IBAction func createNewFolder(_ sender: Any) {
@@ -28,71 +28,52 @@ class MyFoldersTableViewController: UITableViewController {
     @IBAction func createNewImage(_ sender: Any) {
     }
     
+    private func showErrorAlert(text: String) {
+        let alert = UIAlertController(title: "Error", message: text, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return listOfContents.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let item = listOfContents[indexPath.row]
+        
+        var isFolder: ObjCBool = false
+        fileManager.fileExists(atPath: item.path, isDirectory: &isFolder)
+        if isFolder.boolValue {
+            cell.detailTextLabel?.text = "Folder"
+            //         cell.textLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        } else {
+            cell.detailTextLabel?.text = ""
+        }
+        cell.textLabel?.text = item.lastPathComponent
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let item = listOfContents[indexPath.row]
+            do {
+                try fileManager.removeItem(at: item)
+            } catch {
+                showErrorAlert(text: "Unable to delete this item")
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
