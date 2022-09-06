f6bc6be047b95fa178237fda79d35d421e1defb4
Marymezh <maria.mezhova@yahoo.com>
2022-09-06 17:17:53 +0700

    Initial Commit

---
+++ FileManagerStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManagerStoryboard
+//
+//  Created by Мария Межова on 06.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
